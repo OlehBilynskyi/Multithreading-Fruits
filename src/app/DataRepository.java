95a0691e3595be4e64dbc8f3446297d33a04ab72
Oleh Bilynskyi <obslinskij@gmail.com>
2025-05-14 21:36:39 +0300

    Add DataRepository class

---
+++ src/app/DataRepository.java
@@ -0,0 +1,8 @@
+package app;
+//Створюємо клас, який повертає масив фруктів.
+public class DataRepository {
+
+    public String[] getData() {
+        return new String[] {"orange", "apple", "plum", "mango"};
+    }
+}
